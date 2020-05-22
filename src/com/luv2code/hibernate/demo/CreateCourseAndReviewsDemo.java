3945d4aa444f7a08f5f7c0a72b3965e518f28866
Dmitriy <fed3375546@yandex.ru>
2020-05-22 22:12:58 +0300

    @OneToMany Mapping FetchType.LAZY

--- src/com/luv2code/hibernate/demo/CreateCourseAndReviewsDemo.java
+++ src/com/luv2code/hibernate/demo/CreateCourseAndReviewsDemo.java
@@ -7,32 +7,34 @@ import org.hibernate.cfg.Configuration;
 import com.luv2code.hibernate.demo.entity.Course;
 import com.luv2code.hibernate.demo.entity.Instructor;
 import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
 
-public class DeleteCoursesDemo {
+public class CreateCourseAndReviewsDemo {
 
 	public static void main(String[] args) {
 
 		try (SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
 				.addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class)
-				.addAnnotatedClass(Course.class).buildSessionFactory(); Session session = factory.getCurrentSession()) {
+				.addAnnotatedClass(Course.class).addAnnotatedClass(Review.class).buildSessionFactory();
+				Session session = factory.getCurrentSession()) {
 
 			// start a transaction
 			session.beginTransaction();
-			
-			// get the instructor from db
-			int theId = 1;
-			Instructor tempInstructor = session.get(Instructor.class, theId);		
-			
-			System.out.println("luv2code: Instructor: " + tempInstructor);
-		
-			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
-			
+
+			// get the course
+			int theId = 10;
+			Course tempCourse = session.get(Course.class, theId);
+
+			// print the course
+			System.out.println(tempCourse);
+
+			// print the course reviews
+			System.out.println(tempCourse.getReviews());
+
 			// commit transaction
 			session.getTransaction().commit();
-			
-			
-			
-			System.out.println("luv2code: Done!");
+
+			System.out.println("Done!");
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
--- src/com/luv2code/hibernate/demo/entity/Course.java
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -1,20 +1,24 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "course")
 public class Course {
 
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
@@ -27,12 +31,24 @@ public class Course {
 	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
 
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
+
 	public Course() {
 
 	}
 
 	public Course(String title) {
 		this.title = title;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
 	}
 
 	public int getId() {
@@ -57,6 +73,17 @@ public class Course {
 
 	public void setInstructor(Instructor instructor) {
 		this.instructor = instructor;
+	}
+
+	// add a convenience method
+
+	public void addReview(Review theReview) {
+
+		if (reviews == null) {
+			reviews = new ArrayList<>();
+		}
+
+		reviews.add(theReview);
 	}
 
 	@Override
